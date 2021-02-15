737847a9925331a6f591af577fe09b9ac0cbd38f
Dharven Doshi <dharven123@gmail.com>
2021-02-15 11:57:49 +0530

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/com/example/spin_o_jay/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.spin_o_jay;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.spin_o_jay";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
